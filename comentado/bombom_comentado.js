9a2d3644d2f190e102c7bb986adfcf4f6a2fbf39
Victor <101749261+victorrschmidt@users.noreply.github.com>
2022-06-28 01:19:16 -0300

    Add files via upload

---
+++ comentado/bombom_comentado.js
@@ -0,0 +1,47 @@
+// OBI 2022 (Olimpíada Brasileira de Informática)
+// Fase 1
+// Modalidade Programação Nível 2
+// Exercício: Bombom 
+// Solução em Javascript 
+
+var n, edu, lua, c, p, result;
+
+n = 'AJQK'; // Naipes 
+edu = 0; // Pontuação Edu
+lua = 0; // Pontuação Luana
+c = []; // Cartas 
+
+  for(i = 0; i < 7; i++){ // Verificar as cartas
+
+    scanf("%s","c[i]"); // Ler as cartas
+
+    if(i == 0){ // A primeira carta será usada apenas pra verificar o naipe dominante
+      continue; // Logo o registro de pontuação (p) será ignorado para para a primeira carta (célula 0 do array de cartas c) e o loop continua
+    }
+
+    p = 10 + n.indexOf(c[i][0]); // Definir o valor inicial do ponto como: 10 mais o valor do index do naipe na variável n
+
+    if(c[i].indexOf(c[0][1]) != -1){ // Verificar se a carta possui o naipe dominante (verificando o index da primeira carta)
+      p += 4; // Somar pontos extras 
+    }
+         
+    if(i < 4){
+      lua += p; // Adicionar pontos para Luana quando é a vez dela.
+    }
+    else{
+      edu += p; // Adicionar pontos para Edu quando é a vez dele.
+    }
+
+  }
+
+  if(lua > edu){
+    result = 'Luana';
+  }
+  else if(edu > lua){
+    result = 'Edu';
+  }
+  else{
+    result = 'empate';
+  }
+
+printf("%s\n",result);
---
+++ comentado/maior_comentado.js
@@ -0,0 +1,29 @@
+// OBI 2022 (Olimpíada Brasileira de Informática)
+// Fase 1
+// Modalidade Programação Nível 2
+// Exercício: Maior valor 
+// Solução em Javascript 
+
+var n, m, s, i, c, ct, soma;
+
+scanf("%d%d%d","n","m","s"); // Ler os valores
+i = -1; // Definir a variável como (-1) para caso não se encaixar na condição para ser definida como um número dentro do intervalo
+
+  for(c = m; c >= n; c--){ // Verificar cada número dentro do intervalo (do maior ao menor, pois o primeiro valor será necessariamente o maior)
+
+    soma = 0; // Definir a soma como 0 para cada loop
+    ct = c; // ct = c temporária; será utilizada para somar os dígitos do número c
+
+    while(ct != 0){ // Loop enquanto ct não for 0
+      soma += ct % 10; // A soma acrescenta em seu valor ct % 10 (o valor do último dígito)
+      ct = parseInt(ct / 10); // ct é definida como a parte inteira de (ct/10), ou seja, o último dígito é ignorado, assim todos os dígitos serão verificados até ct ser 0
+    }
+
+    if(soma == s){ // Caso a soma for igual a variável s
+      i = c; // O valor de i será igual ao valor atual de c
+      break; // Encerrar o loop, pois o maior valor de c tal que a soma de seus dígitos é igual a s já foi registrado
+    }
+
+  }
+
+printf("%d\n",i);
