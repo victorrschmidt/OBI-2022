bd2d792021162c303d3afe2b3817bb8ff39d0c65
Victor <101749261+victorrschmidt@users.noreply.github.com>
2022-06-28 04:26:41 -0300

    Update hotel_comentado.js

--- comentado/hotel_comentado.js
+++ comentado/hotel_comentado.js
@@ -10,7 +10,7 @@ scanf("%d%d%d","d","a","n"); // Ler os valores
 x = 1; // Valor a ser subtraído do dia em que a pessoa chega
 
 if(n > 15){ // Caso o dia for maior que 15, o valor não é mais aumentado
-  x = n - 14; // Neste caso x será o dia subtraído de 14 (1 dia antes do valor atingir o nível máximo; dia 15)
+  x = n - 14; // Neste caso x será igual ao dia subtraído de 14 (1 dia antes do valor atingir o nível máximo; dia 15)
 }
 
 printf("%d\n",(32-n)*(d+(n-x)*a));
@@ -26,7 +26,7 @@ printf("%d\n",(32-n)*(d+(n-x)*a));
     
    (n - x) -> A quantidade a ser paga (multiplicada) pelo valor de 'a' (o valor que a diária aumenta; com limite de 14)
 
-   (d + (n - x) * a) -> O valor da diária somado a multiplicação da quantidade de dias em que o valor aumenta e o valor do aumento (a)
+   (d + (n - x) * a) -> O valor da diária somado a multiplicação da quantidade de dias em que o valor aumenta e o valor do aumento 'a'
 
 
    (32 - n) * (d + (n - x) * a) -> Por fim, multiplicamos a quantidade de dias pelo valor das diárias
