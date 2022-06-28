9e8a9346046b335c4b503a3b8523da5643dae563
Victor <101749261+victorrschmidt@users.noreply.github.com>
2022-06-28 01:53:33 -0300

    Add files via upload

--- maior.js
+++ maior.js
@@ -1,6 +1,6 @@
 // OBI 2022 (Olimpíada Brasileira de Informática)
 // Fase 1
-// Modalidade Programação Nível 2
+// Modalidade Programação Níveis 2 e Sênior
 // Exercício: Maior valor 
 // Solução em Javascript 
 
